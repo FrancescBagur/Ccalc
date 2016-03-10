a2474627b7298ed9e4d1d4eb4f04471fa9c0466c
MToroV <migueltorov@gmail.com>
2016-03-10 13:21:22 +0100

    cambios layout

--- AndroidEyeCalc/app/src/main/java/com/educem/eyecalc/androideyecalc/MainActivity.java
+++ AndroidEyeCalc/app/src/main/java/com/educem/eyecalc/androideyecalc/MainActivity.java
@@ -22,14 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         //inicialitzo el intent i li dic que vagi d'aquesta activity a la segona.
         inToActivity2 = new Intent(this,Main2Activity.class);
         //associo el boto programatic amb el boto creat visualment
