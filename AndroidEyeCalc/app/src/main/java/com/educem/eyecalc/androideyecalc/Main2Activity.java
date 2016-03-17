7772ed87df0c0c111d08c3e844ae9784a189e32b
Pere Almendro <palmendrob@gmail.com>
2016-03-17 10:53:05 +0100

    canvios api

--- AndroidEyeCalc/app/src/main/java/com/educem/eyecalc/androideyecalc/Main2Activity.java
+++ AndroidEyeCalc/app/src/main/java/com/educem/eyecalc/androideyecalc/Main2Activity.java
@@ -1,6 +1,9 @@
 package com.educem.eyecalc.androideyecalc;
 
 import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.ContentUris;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -17,8 +20,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 //imports per comunicarse amb el servidor
+import com.android.internal.http.multipart.MultipartEntity;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
@@ -27,6 +34,7 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.json.JSONException;
@@ -35,14 +43,17 @@ import org.json.JSONObject;
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main2Activity extends AppCompatActivity {
     //elements per la camara
@@ -51,7 +62,9 @@ public class Main2Activity extends AppCompatActivity {
     Bitmap bmpInvertit;//imatge que senviara al server
     private static final int CAM_REQUEST = 1313;
     //adreça del nostre server
-    private static final String SERVER_ADRESS="http://192.168.0.158/ApiCcalc/";
+    private static final String SERVER_ADRESS="http://192.168.43.101/ApiCcalc/";
+    private File file;
+    private String foto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -99,13 +112,110 @@ public class Main2Activity extends AppCompatActivity {
                 bmpInvertit.setDensity(DisplayMetrics.DENSITY_DEFAULT);
                 //mostra la imatge
                 takenfoto.setImageBitmap(bmpInvertit);
-                //enviar la imatge a la api, cridem a la clase que hem fet i li pasem la imatge i un nom de la imatge.
-                new uploadFile().execute();
+                //enviar la imatge a la api,
+                new ServerUpdate().execute();
             } else if (resultCode == RESULT_CANCELED) Toast.makeText(getApplicationContext(), "el usuari ha cancelat la captura de la imatge", Toast.LENGTH_LONG).show();
             else Toast.makeText(getApplicationContext(), "error en captura la imatge", Toast.LENGTH_LONG).show();
         }
     }
-    // 3 classe para subir la foto al server
+    //CONEXIO AMB SERVIDOR
+    class ServerUpdate extends AsyncTask<String,String,String>{
+        ProgressDialog pDialog;
+        @Override
+        protected String doInBackground(String... arg0) {
+            uploadFoto();
+            //inserta la imatge a base de dades del servidor cridant a un altra plana de la web
+            /*if(onInsert()) runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(Main2Activity.this,"Imatge enviada al servidor",Toast.LENGTH_LONG).show();
+                }
+            });
+            else
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(Main2Activity.this,"Error en enviar imatge al servidor",Toast.LENGTH_LONG).show();
+                    }
+                });*/
+            return null;
+        }
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Main2Activity.this);
+            pDialog.setMessage("Actualitzant servidor, esperi...");
+            pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            pDialog.show();
+        }
+        protected void onPostExecute(String result){
+            super.onPostExecute(result);
+            pDialog.dismiss();
+        }
+        //envia la imatge al servidorr
+        private  void uploadFoto(){
+            HttpClient httpclient = new DefaultHttpClient();
+            httpclient.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+            HttpPost httppost = new HttpPost("http://192.168.0.11/picarcodigo/upload.php");
+            MultipartEntity mpEntity = new MultipartEntity();
+            //download httpmime-4.0-beta 2.jar
+            ContentBody foto = new FileBody(file, "image/jpeg");
+            mpEntity.addPart("fotoUp", foto);
+            httppost.setEntity(mpEntity);
+            try {
+                httpclient.execute(httppost);
+                httpclient.getConnectionManager().shutdown();
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        private HttpParams getHttpRequestParams(){
+            HttpParams HttpRequestParams = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(HttpRequestParams, 1000 * 30);
+            HttpConnectionParams.setSoTimeout(HttpRequestParams,1000*30);
+            return HttpRequestParams;
+        }
+        //enviar la foto a la api perque la guardi a base de dades.
+        private boolean onInsert(){
+            HttpClient httpClient;
+            List<NameValuePair> nameValuePairs;
+            HttpPost httpPost;
+            httpClient=new DefaultHttpClient();
+            httpPost = new HttpPost(SERVER_ADRESS+"index.php");
+            //codifico la imatge
+            //intanciem un byteArrayOutputStream
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            //comprimim la imatge en format JPEG amb cualitat 100 i la guardem al byteArrayOutputStream anterior
+            bmpInvertit.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            //codifiquem la imatge
+            String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(),Base64.DEFAULT);
+            //afegim les dades a enviar
+            nameValuePairs = new ArrayList<NameValuePair>(1);
+            nameValuePairs.add(new BasicNameValuePair("imatge", encodedImage));
+            try{
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                httpClient.execute(httpPost);
+                return true;
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+
+    }
+
+
+
+
+
+    /* 3 classe para subir la foto al server
     public class uploadFile extends AsyncTask <Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
@@ -118,10 +228,10 @@ public class Main2Activity extends AppCompatActivity {
             //creem un arraylist i li afegim les dades que volem enviar al server.
                 /*ArrayList<NameValuePair> dataToSend = new ArrayList<>();
                 dataToSend.add(new BasicNameValuePair("image",encodedImage));
-                dataToSend.add(new BasicNameValuePair("name","primeraImgPalserver"));*/
+                dataToSend.add(new BasicNameValuePair("name","primeraImgPalserver"));
             //instanciem la conexio aki perque el finally la pugui desconectar
             try {
-                URL url = new URL(SERVER_ADRESS+"index.php?img="+encodedImage);
+                URL url = new URL(SERVER_ADRESS+"index.php");
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                 try {
                     //obrim connexio amb el servidor
@@ -148,7 +258,7 @@ public class Main2Activity extends AppCompatActivity {
             }
             return null;
         }
-    }
+    } */
     /* INTENTONAS VARIAS SIN EXITO ALGUNO JODERRRR
     //classe per enviar la imatge al servidor 2
     class ImageUploadTask extends AsyncTask <Void, Void, Void>{
