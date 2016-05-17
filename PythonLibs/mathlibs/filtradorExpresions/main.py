7f68fc0e016e9f5ddfee4508f4efb3a534d97f8a
Pere Almendro <palmendrob@gmail.com>
2016-05-17 16:35:02 +0200

    canvis filtradorExpresions python

---
+++ PythonLibs/mathlibs/filtradorExpresions/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#encoding: latin1
+import sys
+
+def transformarExpresio(self,exp):
+    exp = exp.replace('- \cdots ','+')
+    exp = exp.replace('\cdots - ','+') #mato tots els cdots sense sentit que sol retornar a vegades
+    exp = exp.replace('\cdots ','') #mato tots els cdots sense sentit que sol retornar a vegades
+    exp = exp.replace(',','.')
+    exp = exp.replace('\\pm','+')
+    exp =exp.replace('\pm','+')
+    if "^{-" in (exp):
+        exp = exp.replace('^{-','-')
+        exp = exp.replace('}','')
+
+    return exp
+
+print transformarExpresio(sys.argv[1],sys.argv[1])
