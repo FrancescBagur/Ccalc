ac817e6b3d3db319522b069d4a24f761512e1e33
FrancescBagur <bagur.francesc@gmail.com>
2016-05-22 18:55:23 +0200

    he afegit la web

---
+++ WebCcalc/conclusions.php
@@ -0,0 +1,13 @@
+<html lang="ca">
+	<head><title>Ccalc</title></head>
+	<?php include_once("bootstrap/bootstrap.html"); ?> <!--Incloc les capçaleres de bootstrap-->
+	<link rel="stylesheet" href="css/style.css">
+	<body>
+		<?php include_once("html/navBarIndex.html");?> <!-- Incloc la barra del navegador -->
+		<div class="container">
+			<h1 class="title1">Conclusions</h1>
+			
+		</div>
+		<?php include_once("html/footer.html");?> <!--Incloc el footer de la pàgina -->
+	</body>
+</html>
---
+++ WebCcalc/tecnologies.php
@@ -0,0 +1,87 @@
+<html lang="ca">
+	<head><title>Ccalc</title></head>
+	<?php include_once("bootstrap/bootstrap.html"); ?> <!--Incloc les capçaleres de bootstrap-->
+	<link rel="stylesheet" href="css/style.css">
+	<body>
+		<?php include_once("html/navBarIndex.html");?> <!-- Incloc la barra del navegador -->
+		<div class="container">
+			<h1 class="title1">Tecnologies</h1>
+			<h2 class="title2">Llenguatges de programació</h2>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/java.png" alt="java">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">Java</h4>
+			    Hem utilitzat java per programar l'aplicaci&oacute; Android del dispositiu m&ograve;bil de forma nativa i per desenvolupar el servidor. La comunicaci&oacute; entre aquests dos s'ha fet mitjançant sockets java.
+			  </div>
+			</div>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/python.jpeg" alt="java">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">Python</h4>
+			    Hem utilitzat python per programar les llibreries matem&agrave;tiques ja que aquest ens donava molt alt nivell i a m&eacute;s com esta basat amb c++, si en un futur el volem integrar dins el dispositiu m&ograve;bil, el Java NDK ens ho permet.
+			  </div>
+			</div>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/c++.png" alt="java">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">C++</h4>
+			    C++ s'ha utilitzat per programar coses a més baix nivell, com per exemple un transformador de punts extrets de autotrace per passar-los al format del seshat. Aquest llenguatge també es pot integrar dintre dels dispositius m&ograve;bils Android gr&agrave;cies al java NDK.
+			  </div>
+			</div>
+			<h2 class="title2">IDE utilitzats per desenvolupar</h2>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/androidstudio.jpeg" alt="java">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">AndroidStudio</h4>
+			    Per desenvolupar l'aplicaci&oacute; m&ograve;bil, em utilitzat AndroidStudio, que &eacute;s la proporcionada per Android.
+			  </div>
+			</div>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/monodevelop.png" alt="monodevelop">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">Monodevelop</h4>
+			    Per desenvolupar amb C++ em esollit Monodevelop, la vam escollir despr&eacute;s de fer una cerca per internet i llegir que era de les millors.
+			  </div>
+			</div>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/IJ.png" alt="idea">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">IDEA de Intel IJ</h4>
+			    Per programar el servidor java em utilitzat el ide de Intel IJ idea.
+			  </div>
+			</div>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/pycharm.png" alt="idea">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">Pycharm de Intel IJ</h4>
+			    Per programar les llibreries matem&agrave;tiques amb python  em utilitzat el ide de Intel IJ Pycharm.
+			  </div>
+			</div>
+			<div class="media">
+			  <div class="media-left">
+			      <img class="media-object imgwidth" src="imatges/sublimetext.jpeg" alt="idea">
+			  </div>
+			  <div class="media-body">
+			    <h4 class="media-heading">Sublime Text</h4>
+			    Tamb&eacute; per programar amb Python i per desenvolupar aquesta mateixa web, em utilitzat sublime text.
+			  </div>
+			</div>
+		</div>
+		<?php include_once("html/footer.html");?> <!--Incloc el footer de la pàgina -->
+	</body>
+
+</html>
